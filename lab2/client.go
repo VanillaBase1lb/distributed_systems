9468a5d4198b76f9c3bb46f75e95006b0f88ca31
Yuvraj Mishra <40194399+VanillaBase1lb@users.noreply.github.com>
2022-10-10 16:33:24 +0530

    dist ready

--- lab2/client.go
+++ lab2/client.go
@@ -1,91 +1,76 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"net"
-	"os"
+  "bufio"
+  "fmt"
+  "net"
+  "os"
 )
 
-const (
-	SERVER_HOST = "localhost"
-	SERVER_PORT = "8080"
-	SERVER_TYPE = "tcp"
-)
-
-func read(conn net.Conn, id byte, c chan bool) {
-	for {
-		showPrompt()
-		var keyword string
-		fmt.Scanln(&keyword)
-		if keyword == "exit" {
-			conn.Close()
-			c <- true
-			break
-		} else if keyword == "send" {
-			fmt.Println("Enter receiver ID:")
-			var receiverid byte
-			fmt.Scanln(&receiverid)
-			if receiverid == id {
-				fmt.Println("Cannot send message to self")
-				continue
-			}
-			fmt.Println("Enter message:")
-			inputReader := bufio.NewReader(os.Stdin)
-			msg, err := inputReader.ReadString('\n')
-			if err != nil {
-				panic(err)
-			}
-			conn.Write([]byte{receiverid})
-			conn.Write([]byte(msg))
-		} else {
-			fmt.Println("Invalid keyword")
-		}
-	}
+func r(c net.Conn, client byte, channel chan bool) {
+  for {
+    sp()
+    var k string
+    fmt.Scanln(&k)
+    if k == "quit" {
+      c.Close()
+      channel <- true
+      break
+    } else if k == "start" {
+      fmt.Println("receiver")
+      var rd byte
+      fmt.Scanln(&rd)
+      if rd == client {
+        continue
+      }
+      fmt.Println("chat")
+      ir := bufio.NewReader(os.Stdin)
+      m, e := ir.ReadString('\n')
+      if e != nil {
+        panic(e)
+      }
+      c.Write([]byte{rd})
+      c.Write([]byte(m))
+    } else {
+      fmt.Println("Wrong input")
+    }
+  }
 }
 
-func receive(conn net.Conn, id byte, c chan bool) {
-	for {
-		receiverid := make([]byte, 1)
-		buffer := make([]byte, 1024)
-		_, err := conn.Read(receiverid) // discard receiverid
-		n, err := conn.Read(buffer)
-		if err != nil {
-			os.Exit(0)
-		}
-		if receiverid[0] != id {
-			fmt.Printf("##########\nReceived the following message from Client %d:\n", receiverid[0])
-		} else {
-			fmt.Println("##########\nReceived the following message from Server:")
-		}
-		fmt.Print(string(buffer[:n]))
-		fmt.Println("##########")
-		fmt.Print(">")
-	}
+func collect(c net.Conn, rc byte, channel chan bool) {
+  for {
+    rd := make([]byte, 1)
+    ch := make([]byte, 1024)
+    _, e := c.Read(rd)
+    n, e := c.Read(ch)
+    if e != nil {
+      os.Exit(0)
+    }
+    if rd[0] != rc {
+      fmt.Printf("client %d sent\n", rd[0])
+    } else {
+      fmt.Println("server sent")
+    }
+    fmt.Print(string(ch[:n]))
+    fmt.Println()
+  }
 }
 
-func showPrompt() {
-	fmt.Println("Type 'send' to send message")
-	fmt.Println("Type 'exit' to quit")
-	fmt.Print(">")
+func sp() {
+  fmt.Println("Type 'start' to start message")
+  fmt.Println("Type 'quit' to quit")
 }
 
 func main() {
-	conn, err := net.Dial(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close()
-	// fmt.Println(conn.LocalAddr().String())
-	fmt.Println("Connected to " + SERVER_HOST + ":" + SERVER_PORT)
-	buffer := make([]byte, 1)
-	conn.Read(buffer)
-	var id byte = buffer[0]
-	fmt.Printf("Client ID = %d\n", id)
+  c, _ := net.Dial("tcp", "localhost"+":"+"9873")
+  defer c.Close()
+  b := make([]byte, 1)
+  c.Read(b)
+  var id byte = b[0]
+  fmt.Printf("client %d\n", id)
 
-	c := make(chan bool)
-	go read(conn, id, c)
-	go receive(conn, id, c)
-	<-c
-	<-c
+  channel := make(chan bool)
+  go r(c, id, channel)
+  go collect(c, id, channel)
+  <-channel
 }
--- lab2/server.go
+++ lab2/server.go
@@ -1,70 +1,43 @@
 package main
 
-import (
-	"fmt"
-	"net"
-)
+import "net"
 
-const (
-	SERVER_HOST = "localhost"
-	SERVER_PORT = "8080"
-	SERVER_TYPE = "tcp"
-)
 
-type receiver struct {
-	id   byte
-	conn net.Conn
+type obj struct {
+  cl byte
+  c net.Conn
 }
 
-var id byte = 0
-var conns = make(map[byte]net.Conn)
+var cl byte = 1
+var maps = make(map[byte]net.Conn)
 
-func listen(conn net.Conn, senderid byte, c chan receiver) {
-	for {
-		receiverid := make([]byte, 1)
-		conn.Read(receiverid)
-		msg := make([]byte, 1024)
-		n, err := conn.Read(msg)
-		if err != nil {
-			fmt.Printf("Client ID %d closed\n", senderid)
-			break
-		}
-		// send message to receiverid
-		receiverconn, exists := conns[receiverid[0]]
-		if !exists {
-			errmsg := "Invalid receiver ID"
-			fmt.Println(errmsg)
-			conn.Write([]byte{senderid})
-			conn.Write([]byte(errmsg))
-			continue
-		}
-		receiverconn.Write([]byte{senderid})
-		receiverconn.Write(msg[:n])
-	}
+func openconnect(c net.Conn, s byte, channel chan obj) {
+  for {
+    r := make([]byte, 1)
+    c.Read(r)
+    m := make([]byte, 1024)
+    n, _ := c.Read(m)
+    conobj, b := maps[r[0]]
+    if !b {
+      c.Write([]byte{s})
+      c.Write([]byte("invalid client"))
+      continue
+    }
+    conobj.Write([]byte{s})
+    conobj.Write(m[:n])
+  }
 }
 
 func main() {
-	listener, err := net.Listen(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
-	if err != nil {
-		panic(err)
-	}
-	c := make(chan receiver)
-	for {
-		conn, err := listener.Accept()
-		defer listener.Close()
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("Listening on " + SERVER_HOST + ":" + SERVER_PORT)
-		_, err = conn.Write([]byte{id})
-		if err != nil {
-			panic(err)
-		}
-		senderid := id
-		conns[senderid] = conn
-		fmt.Printf("Client connected with ID = %d\n", id)
-		id = id + 1
-
-		go listen(conn, senderid, c)
-	}
+  l, _ := net.Listen("tcp", "localhost"+":"+"9873")
+  channel := make(chan obj)
+  for {
+    c, _ := l.Accept()
+    defer l.Close()
+    c.Write([]byte{cl})
+    s := cl
+    maps[s] = c
+    cl = cl + 1
+    go openconnect(c, s, channel)
+  }
 }
